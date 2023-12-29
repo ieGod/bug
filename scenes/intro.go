9b932d7ddd012704d79457d38b8489e231048e93
iegod <diegob@gmail.com>
2023-12-29 11:58:54 -0500

    Another cc update.

--- scenes/intro.go
+++ scenes/intro.go
@@ -53,7 +53,7 @@ func (scene *IntroScene) Draw(img *ebiten.Image) {
 
 	text.Draw(img, constants.Strings.PressEnter, fonts.Bugger.Arcade, scene.dimensions.Width/2-100, scene.dimensions.Height/2+200, color.White)
 
-	text.Draw(img, "®2023 bsoft games", fonts.Bugger.Arcade, scene.dimensions.Width/2-92, scene.dimensions.Height/2+250, color.White)
+	text.Draw(img, "®2023 Diego Benavides under bsoft games", fonts.Bugger.Arcade, scene.dimensions.Width/2-225, scene.dimensions.Height/2+250, color.White)
 
 	scene.cycle++
 }
